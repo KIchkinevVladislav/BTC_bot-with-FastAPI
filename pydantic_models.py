f09deb7ddc307b62082c48f7a7195f97db90963b
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2022-12-16 00:17:54 +0300

    running transactions

--- pydantic_models.py
+++ pydantic_models.py
@@ -1,13 +1,11 @@
-from typing import Union
-
 import pydantic
 from datetime import datetime
 
 
 class User(pydantic.BaseModel):
     id: int
-    tg_ID: int
-    nick: str = None
+    tg_ID:  int
+    nick:   str = None
     create_date: datetime
     wallet: 'Wallet'
     sended_transactions: list['Transaction'] = None
@@ -52,34 +50,9 @@ class UserToCreate(pydantic.BaseModel):
     nick:   str = None
 
 
-class CreateTransaction(pydantic.BaseModel):
+class TransactionToCreate(pydantic.BaseModel):
     receiver_address: str
     amount_btc_without_fee: float
-    tg_ID: int = None
-    fee: float = None
-    testnet: bool = True
-
-
-class Token(pydantic.BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(pydantic.BaseModel):
-    username: Union[str, None] = None
-
-
-class Admin(pydantic.BaseModel):
-    username: str
-
-
-class UserInDB(Admin):
-    hashed_password: str
 
 
 UserToUpdate.update_forward_refs()
-User.update_forward_refs()
-UserToCreate.update_forward_refs()
-Transaction.update_forward_refs()
-CreateTransaction.update_forward_refs()
-Wallet.update_forward_refs()
