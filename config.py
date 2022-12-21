d854309d62c9c3fab52a0e3f864ebc19c8ab81bb
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2022-12-21 23:26:45 +0300

    release

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
-IS_TESTNET = True
+# конфигурации нашего приложения
 
-BOT_TOKEN = '' #your bot_token
+IS_TESTNET = True  # определяем работаем ли мы в тестовой сети Биткоин или нет
 
-TG_ADMIN_ID = int() #your telegram id
+BOT_TOKEN = ''  # токен Вашего бота в Телеграм
 
-API_URL = "http://127.0.0.1:8000"
+TG_ADMIN_ID = 724520448  # наш ID в Телеграм
+
+API_URL = 'http://127.0.0.1:8000'
+
+USERNAME = 'admin'  # логин пользователя для аутентификации 
+                    # пароль: admin
+
+PASSWORD = '$2b$12$dW9pYNKell51X8E6qK/yEeCvDaxrWQvsOKXdgZ.gwxq7ubasJQCa2'  # хешированный пароль
+
+# данные для кодировки JWT
+SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiJ9.0OMl6DbUyGidOb3YaVOnqs9vvqTkEzhWrRKvWViyZFU'
+
+ALGORITHM = 'HS256'
