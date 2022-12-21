1b25e4d4e270d82a580c36c0886c776a59f94285
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2022-12-21 23:24:43 +0300

    Update client.py

--- client.py
+++ client.py
@@ -2,26 +2,21 @@ import requests
 import pydantic_models
 from config import API_URL
 
-
-def update_user(user: dict):
-    """Обновляем юзера"""
-    # валидируем данные о юзере, так как мы не под декоратором fastapi и это нужно делать вручную
-    user = pydantic_models.UserToUpdate.validate(user)
-    # чтобы отправить пост запрос - используем метод .post, в аргументе data - отправляем строку в формате json
-    responce = requests.put(f'{API_URL}/user/{user.id}', data=user.json())
-    try:
-        return responce.json()
-    except:
-        return responce.text
+# создаем заголовок в котором указываем, что тип контента - форма
+form_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
 
-def delete_user(user_id: int):
-    """
-    Удаляем юзера
-    :param user_id:
-    :return:
-    """
-    return requests.delete(f'{API_URL}/user/{user_id}').json()
+payload = 'username=admin&password=admin' # здесь наш логин и пароль
+raw_token = requests.post(API_URL+'/token',
+                         headers=form_headers,
+                         data=payload)
+token = raw_token.json()     # получаем словарь из ответа сервера
+sesh = requests.Session()   # создаем экземпляр сессии
+# добавляем хедеры с токеном авторизации, благодаря чему API будет понимать кто мы и возвращать нужные нам ответы
+sesh.headers = {
+      'accept': 'application/json',
+  'Authorization': "Bearer " + token['access_token']
+}
 
 
 def create_user(user: pydantic_models.UserToCreate):
@@ -30,29 +25,80 @@ def create_user(user: pydantic_models.UserToCreate):
     :param user:
     :return:
     """
+    # валидируем данные о пользователе, так как мы не под декоратором fastapi и это нужно делать вручную
     user = pydantic_models.UserToCreate.validate(user)
-    return requests.post(f'{API_URL}/user/create', data=user.json()).json()
+    return sesh.post(f'{API_URL}/user/create', data=user.json()).json()
+
+
+def update_user(user: dict):
+    """
+    Обновляем пользователя
+    :param user:
+    :return:
+    """
+    user = pydantic_models.UserToUpdate.validate(user)
+    responce = sesh.put(f'{API_URL}/user/{user.id}', data=user.json())
+    try:
+        return responce.json()
+    except requests.exceptions.JSONDecodeError:
+        return responce.text
+
+
+def delete_user(user_id: int):
+    """
+    Удаляем пользователя
+    :param user_id:
+    :return:
+    """
+    return sesh.delete(f'{API_URL}/user/{user_id}').json()
+
+
+def get_users():
+    """
+    Получаем всех пользователей
+    :return list:
+    """
+    return sesh.get(f'{API_URL}/users').json()
 
 
 def get_info_about_user(user_id):
     """
-    Получаем инфу по юзеру
+    Получаем информацию о пользователей
     :param user_id:
     :return:
     """
-    return requests.get(f'{API_URL}/get_info_by_user_id/{user_id}').json()
+    return sesh.get(f'{API_URL}/get_info_by_user_id/{user_id}').json()
+
+
+def get_user_by_tg_id(tg_id):
+    """
+    Получаем пользователя по его ID в Телеграм
+    :param tg_id:
+    :return:
+    """
+    return sesh.get(f'{API_URL}/user_by_tg_id/{tg_id}').json()
+
+
+def get_user_wallet_by_tg_id(tg_id):
+    """
+    Получаем данные о кошельке пользователя по его ID в Телеграм
+    :param tg_id:
+    :return:
+    """
+    user_dict = get_user_by_tg_id(tg_id)
+    return sesh.get(f"{API_URL}/get_user_wallet/{user_dict['id']}").json()
 
 
 def get_user_balance_by_id(user_id):
     """
-    Получаем баланс юзера
+    Получаем баланс пользователя
     :param user_id:
     :return:
     """
-    responce = requests.get(f'{API_URL}/get_user_balance_by_id/{user_id}')
+    responce = sesh.get(f'{API_URL}/get_user_balance_by_id/{user_id}')
     try:
         return float(responce.text)
-    except:
+    except TypeError:
         return f'Error: Not a Number\nResponce: {responce.text}'
 
 
@@ -62,110 +108,39 @@ def get_total_balance():
 
     :return:
     """
-    responce = requests.get(f'{API_URL}/get_total_balance')
+    responce = sesh.get(f'{API_URL}/get_total_balance')
     try:
         return float(responce.text)
-    except:
+    except TypeError:
         return f'Error: Not a Number\nResponce: {responce.text}'
 
 
-def get_users():
+def create_transaction(tg_id, receiver_address: str, amount_btc_without_fee: float):
     """
-    Получаем всех юзеров
-    :return list:
-    """
-    return requests.get(f"{API_URL}/users").json()
-
-
-def get_user_wallet_by_tg_id(tg_id):
-    user_dict = get_user_by_tg_id(tg_id)
-    return requests.get(f"{API_URL}/get_user_wallet/{user_dict['id']}").json()
-
-
-def get_user_by_tg_id(tg_id):
-    """
-    Получаем юзера по айди его ТГ
+    Создаем транзакцию
     :param tg_id:
+    :param receiver_address:
+    :param amount_btc_without_fee:
     :return:
     """
-    return requests.get(f"{API_URL}/user_by_tg_id/{tg_id}").json()
-
-
-def create_transaction(tg_id, receiver_address: str, amount_btc_without_fee: float):
     user_dict = get_user_by_tg_id(tg_id)
     payload = {'receiver_address': receiver_address,
                'amount_btc_without_fee': amount_btc_without_fee}
-    responce = requests.post(f"{API_URL}/create_transaction/{user_dict['id']}", json=payload)
+    responce = sesh.post(f"{API_URL}/create_transaction/{user_dict['id']}", json=payload)
     return responce.text
 
 
 def get_user_transactions(user_id):
-    responce = requests.get(f"{API_URL}/get_user_transactions/{user_id}")
+    """
+    Получаем список транзакций пользователя
+    :param user_id:
+    :return:
+    """
+    responce = sesh.get(f'{API_URL}/get_user_transactions/{user_id}')
     try:
         return responce.json()
-    except Exception as E:
-        return f"{responce.text} \n" \
-               f"Exception: {E.args, E.__traceback__}"
+    except ValueError as E:
+        return f'{responce.text} \n' \
+               f'Exception: {E.args, E.__traceback__}'
 
 
-# def update_user(user: dict):
-#     user = pydantic_models.UserToUpdate.validate(user)
-#     response = requests.put(f'{API_URL}/user/{user.id}', data=user.json())
-#     try:
-#         return response.json()
-#     except requests.exceptions.JSONDecodeError:
-#         return response.text
-#
-#
-# def delete_user(user_id: int):
-#     return requests.delete(f'{API_URL}/user/{user_id}').json()
-#
-#
-# def create_user(user: pydantic_models.UserToCreate):
-#     user = pydantic_models.UserToCreate.validate(user)
-#     return requests.post(f'{API_URL}/user/create', data=user.json()).json()
-#
-#
-# def get_info_about_user(user_id):
-#     return requests.get(f'{API_URL}/get_info_by_user_id/{user_id}').json()
-#
-#
-# def get_user_balance_by_id(user_id):
-#     response = requests.get(f'{API_URL}/get_user_balance_by_id/{user_id}')
-#     try:
-#         return float(response.text)
-#     except TypeError:
-#         return f'Error: Not a Number\nResponse: {response.text}'
-#
-#
-# def get_total_balance():
-#     response = requests.get(f'{API_URL}/get_total_balance')
-#     try:
-#         return float(response.text)
-#     except TypeError:
-#         return f'Error: Not a Number\nResponse: {response.text}'
-#
-#
-# def get_users():
-#     return requests.get(f"{API_URL}/users").json()
-#
-#
-# def get_user_wallet_by_tg_id(tg_id):
-#     user_dict = get_user_by_tg_id(tg_id)
-#     return requests.get(f"{API_URL}/get_user_wallet/{user_dict['id']}").json()
-#
-#
-# def get_user_by_tg_id(tg_id):
-#     return requests.get(f"{API_URL}/user_by_tg_id/{tg_id}").json()
-#
-#
-# def create_transaction(tg_id, receiver_address: str, amount_btc_without_fee: float):
-#     user_dict = get_user_by_tg_id(tg_id)
-#     payload = {'receiver_address': receiver_address,
-#                'amount_btc_without_fee': amount_btc_without_fee}
-#     response = requests.post(f"{API_URL}/create_transaction/{user_dict['id']}", json=payload)
-#     return response.text
-#
-#
-# def get_user_transactions(user_id: int):
-#     return requests.get(f"{API_URL}/get_user_transactions/{user_id}").json()
