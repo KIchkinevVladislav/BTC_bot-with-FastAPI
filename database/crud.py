94d4fea3a0dd6c5d7f1c863182fa21a06e1fd1b4
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2022-12-21 23:23:22 +0300

    release

--- database/crud.py
+++ database/crud.py
@@ -6,25 +6,61 @@ import config
 
 
 @db_session
-def create_wallet(user: pydantic_models.User = None, private_key: str = None, testnet: bool = config.IS_TESTNET):
+def create_wallet(
+        user: pydantic_models.User = None,
+        private_key: str = None,
+        testnet: bool = config.IS_TESTNET
+):
+    """
+    Cоздание кошелька пользователя
+    :param user:
+    :param private_key:
+    :param testnet: в тестовой сети ли мы работаем или нет
+    :return: Wallet object
+    """
     if not testnet:
         raw_wallet = bit.Key() if not private_key else bit.Key(private_key)
     else:
         raw_wallet = bit.PrivateKeyTestnet() if not private_key else bit.PrivateKeyTestnet(private_key)
     if user:
-        wallet = Wallet(user=user, private_key=raw_wallet.to_wif(), address=raw_wallet.address)
+        wallet = Wallet(
+            user=user,
+            private_key=raw_wallet.to_wif(),
+            address=raw_wallet.address
+        )
     else:
-        wallet = Wallet(private_key=raw_wallet.to_wif(), address=raw_wallet.address)
+        wallet = Wallet(
+            private_key=raw_wallet.to_wif(),
+            address=raw_wallet.address
+        )
     flush()
     return wallet
 
 
 @db_session
-def create_user(tg_id: int, nick: str = None):
+def create_user(
+        tg_id: int,
+        nick: str = None
+):
+    """
+    Создание пользователя
+    :param tg_id: обязательный параметр (id в Телеграм)
+    :param nick:
+    :return: User object
+    """
     if nick:
-        user = User(tg_ID=tg_id, nick=nick, create_date=datetime.now(), wallet=create_wallet())
+        user = User(
+            tg_ID=tg_id,
+            nick=nick,
+            create_date=datetime.now(),
+            wallet=create_wallet()
+        )
     else:
-        user = User(tg_ID=tg_id, create_date=datetime.now(), wallet=create_wallet())
+        user = User(
+            tg_ID=tg_id,
+            create_date=datetime.now(),
+            wallet=create_wallet()
+        )
     flush()
     return user
 
@@ -38,23 +74,21 @@ def create_transaction(
     testnet: bool = config.IS_TESTNET
 ):
     """
+    Создание транзакции
+    :param sender: объект отправителя
     :param amount_btc_without_fee:  количество биткоинов исключая комиссию, значение в сатоши
     :param receiver_address: адрес получателя, строка с адресом
-    :param amount_btc_with_fee: количество биткоинов включая комиссию, значение в сатоши
     :param fee: абсолютная комиссия, исчисляем в сатоши - необязательно.
     :param testnet: в тестовой сети ли мы работаем
     :return: Transaction object
     """
-
-    # тут мы загружаем в переменную wallet_of_sender кошелек отправителя
-    # и если мы в тестовой сети, то соответственно мы загружаем кошелек из тестовой сети
     wallet_of_sender = bit.Key(sender.wallet.private_key) if not testnet else bit.PrivateKeyTestnet(sender.wallet.private_key)
     sender.wallet.balance = wallet_of_sender.get_balance()  # Получаем баланс кошелька
     if not fee:
         fee = bit.network.fees.get_fee() * 1000     # получаем стоимость транзакции sat/B и умножаем на 1000
     amount_btc_with_fee = amount_btc_without_fee + fee  # находим сумму включая комиссию
     if amount_btc_without_fee + fee > sender.wallet.balance:
-        return f"Too low balance: {sender.wallet.balance}"
+        return f'Too low balance: {sender.wallet.balance}'  # проверяем хватает ли средств в кошельки для отправки
 
     # подготавливаем кортеж в списке с данными для транзакции
     output = [(receiver_address, amount_btc_without_fee, 'satoshi')]
@@ -77,6 +111,11 @@ def create_transaction(
 
 @db_session
 def update_wallet_balance(wallet: pydantic_models.Wallet):
+    """
+    Обновление состояния кошелька
+    :param wallet:
+    :return:
+    """
     testnet = False if not wallet.private_key.startswith('c') else True
     bit_wallet = bit.Key(wallet.private_key) if not testnet else bit.PrivateKeyTestnet(wallet.private_key)
     wallet.balance = bit_wallet.get_balance()
@@ -85,6 +124,11 @@ def update_wallet_balance(wallet: pydantic_models.Wallet):
 
 @db_session
 def update_all_wallets():
+    """
+    Обновление состояние всех кошельков
+    При приходе по базе кошельков печает адрес и баланс каждого
+    :return:
+    """
     for wallet in select(w for w in Wallet)[:]:
         update_wallet_balance(wallet)
         print(wallet.address, wallet.balance)
@@ -93,55 +137,85 @@ def update_all_wallets():
 
 @db_session
 def get_user_by_id(id: int):
+    """
+    Получаев пользователя
+    :param id: id объекта пользователя в базе данных
+    :return: User object
+    """
     return User[id]
 
 
 @db_session
 def get_user_by_tg_id(tg_id: int):
+    """
+    Получаем пользователя по его ID в Телеграм
+    :param tg_id:
+    :return: User object
+    """
     return User.select(lambda u: u.tg_ID == tg_id).first()
 
 
 @db_session
 def get_transaction_info(transaction: pydantic_models.Transaction):
-    return {"id": transaction.id,
-            "sender": transaction.sender if transaction.sender else None,
-            "receiver": transaction.receiver if transaction.receiver else None,
-            "sender_wallet": transaction.sender_wallet if transaction.sender_wallet else None,
-            "receiver_wallet": transaction.receiver_wallet if transaction.receiver_wallet else None,
-            "sender_address": transaction.sender_address,
-            "receiver_address": transaction.receiver_address,
-            "amount_btc_with_fee": transaction.amount_btc_with_fee,
-            "amount_btc_without_fee": transaction.amount_btc_without_fee,
-            "fee": transaction.fee,
-            "date_of_transaction": transaction.date_of_transaction,
-            "tx_hash": transaction.tx_hash}
+    """
+    Получаем словарь с атрибутами объекта транзакции
+    :param transaction: Transaction object
+    :return:
+    """
+    return {'id': transaction.id,
+            'sender': transaction.sender if transaction.sender else None,
+            'receiver': transaction.receiver if transaction.receiver else None,
+            'sender_wallet': transaction.sender_wallet if transaction.sender_wallet else None,
+            'receiver_wallet': transaction.receiver_wallet if transaction.receiver_wallet else None,
+            'sender_address': transaction.sender_address,
+            'receiver_address': transaction.receiver_address,
+            'amount_btc_with_fee': transaction.amount_btc_with_fee,
+            'amount_btc_without_fee': transaction.amount_btc_without_fee,
+            'fee': transaction.fee,
+            'date_of_transaction': transaction.date_of_transaction,
+            'tx_hash': transaction.tx_hash}
 
 
 @db_session
 def get_wallet_info(wallet: pydantic_models.Wallet):
+    """
+    Получаем словарь с атрибутами объекта кошелька
+    :param wallet: Wallet object
+    :return:
+    """
     wallet = update_wallet_balance(wallet)
-    return {"id": wallet.id if wallet.id else None,
-            "user": wallet.user if wallet.user else None,
-            "balance": wallet.balance if wallet.balance else 0.0,
-            "private_key": wallet.private_key if wallet.private_key else None,
-            "address": wallet.address if wallet.address else None,
-            "sended_transactions": wallet.sended_transactions if wallet.sended_transactions else [],
-            "received_transactions": wallet.received_transactions if wallet.received_transactions else []}
+    return {'id': wallet.id if wallet.id else None,
+            'user': wallet.user if wallet.user else None,
+            'balance': wallet.balance if wallet.balance else 0.0,
+            'private_key': wallet.private_key if wallet.private_key else None,
+            'address': wallet.address if wallet.address else None,
+            'sended_transactions': wallet.sended_transactions if wallet.sended_transactions else [],
+            'received_transactions': wallet.received_transactions if wallet.received_transactions else []}
 
 
 @db_session
 def get_user_info(user: pydantic_models.User):
-    return {"id": user.id,
-            "tg_ID": user.tg_ID if user.tg_ID else None,
-            "nick": user.nick if user.nick else None,
-            "create_date": user.create_date,
-            "wallet": get_wallet_info(user.wallet),
-            "sended_transactions": user.sended_transactions if user.sended_transactions else [],
-            "received_transactions": user.received_transactions if user.received_transactions else []}
+    """
+    Получаем словарь с атрибутами объекта Пользователя
+    :param user: User object
+    :return:
+    """
+    return {'id': user.id,
+            'tg_ID': user.tg_ID if user.tg_ID else None,
+            'nick': user.nick if user.nick else None,
+            'create_date': user.create_date,
+            'wallet': get_wallet_info(user.wallet),
+            'sended_transactions': user.sended_transactions if user.sended_transactions else [],
+            'received_transactions': user.received_transactions if user.received_transactions else []}
 
 
 @db_session
 def update_user(user: pydantic_models.UserToUpdate):
+    """
+    Обновляем данные в модели пользователя
+    :param user:
+    :return:
+    """
     user_to_update = User[user.id]
     if user.tg_ID:
         user_to_update.tg_ID = user.tg_ID
@@ -156,6 +230,11 @@ def update_user(user: pydantic_models.UserToUpdate):
 
 @db_session
 def get_user_transactions(user_id):
+    """
+    Получаем список транзакций пользователя
+    :param user_id:
+    :return:
+    """
     return list(map(lambda t: t.to_dict(),
                     Transaction.select(lambda trans: trans.sender.id == user_id or trans.receiver.id == user_id)[:]))
 
