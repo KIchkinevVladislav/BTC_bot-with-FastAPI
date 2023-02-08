99685e072329927a970fae7cfe3c181cbf7a75d7
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-02-08 20:58:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,9 @@ TG_ADMIN_ID = 724520448  # наш ID в Телеграм
 
 API_URL = 'http://127.0.0.1:8000'
 
-USERNAME = 'admin'  # логин пользователя для аутентификации 
-                    # пароль: admin
+USERNAME = 'admin'  # логин пользователя для аутентификации                 
 
-PASSWORD = '$2b$12$dW9pYNKell51X8E6qK/yEeCvDaxrWQvsOKXdgZ.gwxq7ubasJQCa2'  # хешированный пароль
+PASSWORD = '$2b$12$dW9pYNKell51X8E6qK/yEeCvDaxrWQvsOKXdgZ.gwxq7ubasJQCa2'  # хешированный пароль: admin
 
 # данные для кодировки JWT
 SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiJ9.0OMl6DbUyGidOb3YaVOnqs9vvqTkEzhWrRKvWViyZFU'
