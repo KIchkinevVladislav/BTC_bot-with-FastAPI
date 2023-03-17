5834f707d62b75d09874f318698f0c30f891ca04
Kichkinev Vladislav <100088832+KIchkinevVladislav@users.noreply.github.com>
2023-03-17 23:05:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ API_URL = 'http://127.0.0.1:8000'
 
 USERNAME = 'admin'  # логин пользователя для аутентификации                 
 
-PASSWORD = '$2b$12$dW9pYNKell51X8E6qK/yEeCvDaxrWQvsOKXdgZ.gwxq7ubasJQCa2'  # хешированный пароль: admin
+PASSWORD = '$2b$12$dW9pYNKell51X8E6qK/yEeCvDaxrWQvsOKXdgZ.gwxq7ubasJQCa2'  
+# хешированный пароль: admin
 
 # данные для кодировки JWT
 SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJhZG1pbiJ9.0OMl6DbUyGidOb3YaVOnqs9vvqTkEzhWrRKvWViyZFU'
